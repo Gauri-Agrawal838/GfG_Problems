cfe2d50fd9852750d56a697db4b985e29f8ab70b
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2023-06-20 12:33:03 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum X (xor) A/minimum-x-xor-a.java
+++ Medium/Minimum X (xor) A/minimum-x-xor-a.java
@@ -31,90 +31,29 @@ class GFG {
 class Solution {
     public static int minVal(int a, int b) {
         // code here
-        int csb = 0, csa = 0; 
-
-        for (int i = 0; i < 31; i++) {
-
-            if ((b & (1 << i)) != 0) {
-
-                csb++;
-
+        int bitsA = Integer.bitCount(a);
+        int bitsB = Integer.bitCount(b);
+        
+        int diff = Math.abs(bitsA-bitsB);
+        
+        if(diff==0)
+            return a;
+        else if(bitsA>bitsB)
+        {
+            while(diff>0)
+            {
+                a = a&(a-1);
+                diff--;
             }
-
-            if ((a & (1 << i)) != 0) {
-
-                csa++; 
-
-            }
-
         }
-
-        int ans = 0; 
-
-        if (csb < csa) {
-
-            for (int i = 31; i >= 0; i--) {
-
-                if ((a & (1 << i)) != 0) {
-
-                    ans += (1 << i);
-
-                    csb--;
-
-                }
-
-                if (csb == 0) {
-
-                    break;
-
-                }
-
+        else
+        {
+            while(diff>0)
+            {
+                a = a|(a+1);
+                diff--;
             }
-
-            return ans;
-
-        } else if (csb == csa) {
-
-            return a; 
-
-        } else {
-
-            java.util.Map<Integer, Integer> m = new java.util.HashMap<>();
-
-            for (int i = 0; i < 31; i++) {
-
-                if ((a & (1 << i)) != 0) {
-
-                    m.put(i, 1); 
-
-                }
-
-            }
-
-            int len = csb - csa; 
-
-            ans = a;
-
-            for (int i = 0; i < 31; i++) {
-
-                if (!m.containsKey(i)) {
-
-                    len--;
-
-                    ans += (1 << i); 
-
-                }
-
-                if (len == 0) {
-
-                    break;
-
-                }
-
-            }
-
-            return ans;
-
         }
+        return a;
     }
 }
