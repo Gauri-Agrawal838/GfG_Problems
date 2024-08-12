8ddeae2e00e404fc6bea76dbefd86515a01c15a7
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2024-08-12 18:03:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Middle elements of two sorted arrays/sum-of-middle-elements-of-two-sorted-arrays.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    public int SumofMiddleElements(int[] arr1, int[] arr2) {
+        // code here
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<arr1.length;i++)
+            list.add(arr1[i]);
+        for(int i=0;i<arr2.length;i++)
+            list.add(arr2[i]);
+            
+        Collections.sort(list);
+        int val1 = list.get(list.size()/2 - 1);
+        int val2 = list.get(list.size()/2);
+        return val1+val2;
+    }
+}
+
+//{ Driver Code Starts.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume newline
+        while (t-- > 0) {
+            String[] input1 = sc.nextLine().split(" ");
+            int[] arr = new int[input1.length];
+            for (int i = 0; i < input1.length; i++) {
+                arr[i] = Integer.parseInt(input1[i]);
+            }
+
+            String[] input2 = sc.nextLine().split(" ");
+            int[] brr = new int[input2.length];
+            for (int i = 0; i < input2.length; i++) {
+                brr[i] = Integer.parseInt(input2[i]);
+            }
+
+            Solution ob = new Solution();
+            int res = ob.SumofMiddleElements(arr, brr);
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
