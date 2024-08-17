3118297d6f4cb300bde90de50ce34a24be2be546
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2024-08-18 00:20:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split an array into two equal Sum subarrays/split-an-array-into-two-equal-sum-subarrays.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+
+            int[] arr = new int[s.length];
+            for (int i = 0; i < arr.length; i++) arr[i] = Integer.parseInt(s[i]);
+
+            Solution obj = new Solution();
+            boolean res = obj.canSplit(arr);
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public boolean canSplit(int arr[]) {
+        // code here
+        int allsum=0, targetsum=0;
+        for(int x:arr)
+            allsum += x;
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            allsum -= arr[i];
+            targetsum += arr[i];
+            if(allsum==targetsum)
+                return true;
+        }
+        return false;
+    }
+}
