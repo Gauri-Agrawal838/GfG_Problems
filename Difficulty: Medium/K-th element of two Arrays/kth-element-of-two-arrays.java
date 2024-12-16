b21fcf328302338e2e508d1df71b784244032ca6
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2024-12-16 23:55:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -13,19 +13,20 @@ class GFG {
             int k = Integer.parseInt(br.readLine().trim());
 
             String[] line1 = br.readLine().trim().split(" ");
-            int[] arr1 = new int[line1.length];
+            int[] a = new int[line1.length];
             for (int i = 0; i < line1.length; i++) {
-                arr1[i] = Integer.parseInt(line1[i]);
+                a[i] = Integer.parseInt(line1[i]);
             }
 
             String[] line2 = br.readLine().trim().split(" ");
-            int[] arr2 = new int[line2.length];
+            int[] b = new int[line2.length];
             for (int i = 0; i < line2.length; i++) {
-                arr2[i] = Integer.parseInt(line2[i]);
+                b[i] = Integer.parseInt(line2[i]);
             }
 
             Solution ob = new Solution();
-            System.out.println(ob.kthElement(k, arr1, arr2));
+            System.out.println(ob.kthElement(a, b, k));
+            System.out.println("~");
         }
     }
 }
@@ -36,22 +37,25 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    public long kthElement(int k, int arr1[], int arr2[]) {
+    public int kthElement(int a[], int b[], int k) {
         // code here
-        int n=arr1.length, m=arr2.length;
-        int temp[] = new int[n+m];
-        int i=0, j=0;
-        for(i=0;i<n;i++)
-            temp[i] = arr1[i];
+        int arr[]=new int[a.length+b.length];
+        int lena=a.length;
+        int lenb=b.length;
+        if(a.length<=0  ){
+            return  0;
+        }
+        if(b.length<=0){
+            return 0;
+        }
         
-        for(j=0;j<m;j++)
-            temp[j+i] = arr2[j];
-            
-        Arrays.sort(temp);
-        for(int z=0;z<temp.length;z++)
-        {
-            if(k-1==z)
-                return temp[z];
+        System.arraycopy(a,0,arr,0,lena);
+        System.arraycopy(b,0,arr,lena,lenb);
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            if(i==k-1){
+                return arr[i];
+            }
         }
         return -1;
     }
