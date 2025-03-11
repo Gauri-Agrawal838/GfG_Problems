9979a6003013c6d4c03b43a680d033db24e78804
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2025-03-11 21:36:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
@@ -2,33 +2,34 @@
 import java.io.*;
 import java.util.*;
 
-
 class GFG {
-	public static void main (String[] args) {
-		
-		//taking input using Scanner class
-		Scanner sc = new Scanner(System.in);
-		
-		//taking total testcases
-		int t = sc.nextInt();
-		
-		while(t-- > 0){
-		    
-		    //taking count of stairs
-		    int m = sc.nextInt();
-		    
-		    //creating an object of class DynamicProgramming
-		    Solution obj = new Solution();
-		    
-		    //calling method countWays() of class
-		    //DynamicProgramming
-		    System.out.println(obj.countWays(m));
-		    
-		}
-		
-	}
+    public static void main(String[] args) {
+
+        // taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // taking total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            // taking count of stairs
+            int m = sc.nextInt();
+
+            // creating an object of class DynamicProgramming
+            Solution obj = new Solution();
+
+            // calling method countWays() of class
+            // DynamicProgramming
+            System.out.println(obj.countWays(m));
+
+            System.out.println("~");
+        }
+    }
 }
 // } Driver Code Ends
+
+
 
 
 class Solution
@@ -38,13 +39,22 @@ class Solution
     {
         
         // your code here
-        int mod = 1000000007;
-        int dp[] = new int[n+1];
+         if(n==1) {
+            return 1;
+        }
+        if(n==2) {
+            return 2;
+        }
+        
+        int dp[] = new int[n];
+        
         dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2;i<=n;i++)
-            dp[i] = (dp[i-1] + dp[i-2])%mod;
-        return dp[n]%mod;
+        dp[1] = 2;
+        
+        for(int i =2;i<n;i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n-1];
     }
 }
-
