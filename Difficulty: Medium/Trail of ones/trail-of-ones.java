067da6bc437f53d947dc298773eff321d171be76
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2025-07-11 09:10:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Trail of ones/trail-of-ones.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countConsec(int n) {
+        // code here
+        int total = 1 << n; // 2^n
+
+        int[] dp = new int[n + 2]; 
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return total - dp[n];
+    }
+}
