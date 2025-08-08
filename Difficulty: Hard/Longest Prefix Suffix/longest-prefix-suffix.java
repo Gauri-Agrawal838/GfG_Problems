0b316e0b8bc749e5f43abf92ef468e6793c7d519
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2025-08-08 23:19:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.java
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.java
@@ -1,28 +1,5 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-
-            String s = read.readLine();
-            Solution ob = new Solution();
-            System.out.println(ob.lps(s));
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    int lps(String str) {
+    int getLPSLength(String str) {
         // code here
         int n=str.length();
         
