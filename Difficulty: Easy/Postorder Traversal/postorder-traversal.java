60a702ce3bc27298ebde7e286bd0b26c41b70a98
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2025-10-09 10:20:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Postorder Traversal/postorder-traversal.java
@@ -0,0 +1,29 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+    Node(int val){
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public ArrayList<Integer> postOrder(Node root) {
+        // code here
+        ArrayList<Integer> postorder = new ArrayList<>();
+        if(root==null)return postorder;
+        Stack<Node> stack1=new Stack<>();
+        Stack<Node> stack2=new Stack<>();
+        stack1.push(root);
+        while(!stack1.isEmpty()){
+            root=stack1.pop();
+            stack2.push(root);
+            if(root.left!=null)stack1.push(root.left);
+            if(root.right!=null)stack1.push(root.right);
+        }
+        while(!stack2.isEmpty())postorder.add(stack2.pop().data);
+        return postorder;
+    }
+}
