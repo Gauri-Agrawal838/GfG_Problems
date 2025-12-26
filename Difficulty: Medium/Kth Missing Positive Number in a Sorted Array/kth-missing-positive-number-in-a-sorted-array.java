a60609f327a33a402dcd357f92f8dc49961ac5ef
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2025-12-26 22:11:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -1,49 +1,15 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.kthMissing(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     public int kthMissing(int[] arr, int k) {
         // code here
-        int missCount=0, curr=1, idx=0;
-        while(true)
-        {
-            if(idx<arr.length && arr[idx]==curr)
-                idx++;
-            else
-            {
-                missCount++;
-                if(missCount==k)
-                    return curr;
+        ArrayList<Integer> res=new ArrayList<>();
+        int j=0;
+        int l=arr[arr.length-1];
+        for(int i=1;res.size()<k;i++){
+            if(j<arr.length && arr[j]==i){ j++;}
+            else{
+                res.add(i);
             }
-            curr++;
         }
+        return res.get(k-1);
     }
 }
